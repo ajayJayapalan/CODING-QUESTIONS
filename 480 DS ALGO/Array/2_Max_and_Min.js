c4e5be86ed1d948aa2726ca7b164461b01157082
ajayJayapalan <ajai.jayapalan@gmail.com>
2021-11-04 06:55:07 +0530

    :)

--- 480 DS ALGO/Array/2_Max_and_Min.js
+++ 480 DS ALGO/Array/2_Max_and_Min.js
@@ -17,3 +17,43 @@
 
 // t : o(n)
 // s : o(1)
+
+// 2
+function getMinMax(arr, n) {
+  let minmax = {};
+  var i;
+
+  if (n == 1) {
+    minmax.max = arr[0];
+    minmax.min = arr[0];
+    return minmax;
+  }
+
+  /* If there are more than one elements, then initialize min
+	and max*/
+
+  if (arr[0] > arr[1]) {
+    minmax.max = arr[0];
+    minmax.min = arr[1];
+  } else {
+    minmax.max = arr[1];
+    minmax.min = arr[0];
+  }
+
+  for (i = 2; i < n; i++) {
+    if (arr[i] > minmax.max) {
+      minmax.max = arr[i];
+    } else if (arr[i] < minmax.min) {
+      minmax.min = arr[i];
+    }
+  }
+
+  return minmax;
+}
+
+/* Driver program to test above function */
+
+var arr = [1000, 11, 445, 1, 330, 3000];
+var arr_size = 6;
+minmax = getMinMax(arr, arr_size);
+console.log(minmax);
