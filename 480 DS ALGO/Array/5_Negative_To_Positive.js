c5b882788f1b67a833e70a560cfc5e449cd7ad4f
ajayJayapalan <ajai.jayapalan@gmail.com>
2021-11-05 21:55:17 +0530

    .

---
+++ 480 DS ALGO/Array/5_Negative_To_Positive.js
@@ -0,0 +1,21 @@
+// Move all negative numbers to beginning and positive to end with constant extra space
+
+/* 
+    time : O(n)
+    space : O(1)
+*/
+
+const swap = (e1, e2) => [e2, e1];
+
+const negToPost = (arr) => {
+  let j = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      [arr[j], arr[i]] = swap(arr[j], arr[i]);
+      j++;
+    }
+  }
+  console.log(arr);
+};
+
+negToPost([-12, 11, -13, -5, 6, -7, 5, -3, -6]);
