5e8206cd4c1352f5adf09d932bfda5c434fc170d
ajayJayapalan <ajai.jayapalan@gmail.com>
2021-11-10 19:04:37 +0530

    .

--- 480 DS ALGO/Array/13_Merge_Intervals.js
+++ 480 DS ALGO/Array/13_Merge_Intervals.js
@@ -6,6 +6,13 @@ Output: [[1,6],[8,10],[15,18]]
 Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
 */
 
+/* 
+
+  time: O(n)
+  space: O(n)
+
+*/
+
 const input = [
   [1, 3],
   [2, 6],
